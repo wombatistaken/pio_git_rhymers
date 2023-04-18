aaa4860df5e4b1fbe6aaa7b6d59ce040edf18364
Rzymon <240772@edu.p.lodz.pl>
2023-04-18 11:17:29 +0200

    2.Hanoi Rhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,14 +4,14 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 
 int totalRejected = 0;
 
-    public int reportRejected() {
-        return totalRejected;
-    }
+public int reportRejected() {
+    return totalRejected;
+}
 
-    public void countIn(int in) {
+public void countIn(int in) {
     if (!callCheck() && in > peekaboo())
             totalRejected++;
-            else
-                super.countIn(in);
-    }
+        else
+            super.countIn(in);
+}
 }
