88481fa48a9e1a9535c324fada890b0352c61354
AllirWasTaken <dawidus250@gmail.com>
2023-04-24 15:33:42 +0200

    6+7+8

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int INT = 12;
+    public static final int INT1 = -1;
+    private final int[] numbers = new int[INT];
 
-    public int total = -1;
+    public int total = INT1;
 
     public void countIn(int in) {
         if (!isFull())
