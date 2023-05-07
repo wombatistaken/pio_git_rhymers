e59c1ed4bd9346558e6c053de7f8033b3cd299b4
tp19121909 <mk.19121909@gmail.com>
2023-05-07 13:30:25 +0200

    9.Widoczność atrybutów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
