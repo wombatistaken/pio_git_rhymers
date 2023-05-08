75a8927b461a527420b73e54f93411f205ff92c7
Allir <97488183+AllirWasTaken@users.noreply.github.com>
2023-05-08 10:47:07 +0200

    Fixes

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    public static final int ERROR_VALUE = -1;
     Node last;
     int i;
 
@@ -9,9 +10,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -25,15 +26,15 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
-        return last.value;
+            return ERROR_VALUE;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
-        int ret = last.value;
-        last = last.prev;
+            return ERROR_VALUE;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
 
